6a7fe55d52cec8aefd041b4406309ae66c8d053c
odrazilef <felix_cuevas@hotmail.com>
2015-12-07 16:13:04 -0800

    Update views.py

--- views.py
+++ views.py
@@ -4,4 +4,4 @@ from django.http import HttpResponse
 # Create your views here.
 
 def primer_vista(request):#siempre recibe un parámetro HttpRequest
-    return HttpResponse("Hola, soy tu primera vista")
+    return HttpResponse("Hola, soy tu primer proyecto en Django")
